91a7e07367a5e804cc0dfd307e9736f6b87fcf45
Max Base <MaxBaseCode@gmail.com>
2021-07-17 20:16:35 +0430

    Apply a change

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#index'
-  get 'new', to: 'homes#new'
-
+  # get 'new', to: 'homes#new'
+  get '/new' => 'homes#new', :defaults => { :name => 'Max' }
+  get '/new/:name' => 'homes#new'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_14_123219) do
+ActiveRecord::Schema.define(version: 2021_07_17_152719) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.string "author"
+    t.string "pages"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "ip"
